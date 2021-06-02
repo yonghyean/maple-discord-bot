a5732c7c7f5fd79cb2dffc02b843f93d1626b703
derek <derek@cemware.com>
2021-06-02 17:38:10 +0900

    test puppeteer

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const data = require("./data.json");
+// const data = require("./data.json");
 const hunting = require("./hunting.json");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const nodeHtmlToImage = require('node-html-to-image')
+const puppeteer = require("puppeteer");
 
 const maple_gg_user = 'https://maple.gg/u/';
 const getHtml = async (url) => {
@@ -46,23 +47,52 @@ client.on('message', async (msg) => {
       `);
         return;
       }
-      const data = await getHtml(maple_gg_user + encodeURI(info));
-      const $ = cheerio.load(data.data, {decodeEntities: true});
-      const h3 = $("section.container > h3");
-        if (h3 && h3.text().indexOf('검색결과가 없습니다.') !== -1) {
-          msg.reply(`
-          > 검색결과가 없습니다.
-          `);
-        } else {
-          // TODO 캐릭터 정보 꾸미기
-          const characterCardHtml = $('#character-card').html();
-          const image = await nodeHtmlToImage({
-            html: characterCardHtml,
-            puppeteerArgs: {executablePath: "chromium-browser", args: ["--no-sandbox", "--disable-setuid-sandbox"]},
-          });
-          const discordSendImage = new Discord.MessageAttachment(image);
-          msg.channel.send(discordSendImage);
+      const browser = await puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        executablePath: "chromium-browser"
+        
+      });       // run browser
+      const page = await browser.newPage();           // open new tab
+      await page.goto(maple_gg_user + encodeURI(info));          // go to site
+      await page.waitForSelector('button[data-target="#exampleModal"]', {
+        visible: true,
+      });
+      await page.click('button[data-target="#exampleModal"]');
+      await page.waitForSelector('#character-card',{
+        visible: true,
+      });          // wait for the selector to load
+      const elem = await page.$('#character-card');
+      const bounding_box = await elem.boundingBox(); 
+      const base64Image = await elem.screenshot({
+        encoding: 'base64',
+        clip: {
+          width: bounding_box.width,
+          height: bounding_box.height,
+          x: bounding_box.x,
+          y: 0,
         }
+      })  
+      const discordSendImage = new Discord.MessageAttachment(base64Image);
+      msg.channel.send(discordSendImage);
+      await browser.close();   
+      // const data = await getHtml(maple_gg_user + encodeURI(info));
+      // const $ = cheerio.load(data.data, {decodeEntities: true});
+      // const h3 = $("section.container > h3");
+      //   if (h3 && h3.text().indexOf('검색결과가 없습니다.') !== -1) {
+      //     msg.reply(`
+      //     > 검색결과가 없습니다.
+      //     `);
+      //   } else {
+      //     // TODO 캐릭터 정보 꾸미기
+      // const characterCardHtml = $('#character-card').html();
+      // const image = await nodeHtmlToImage({
+      //   html: characterCardHtml,
+      //   puppeteerArgs: {executablePath: "chromium-browser", args: ["--no-sandbox", "--disable-setuid-sandbox"]},
+      // });
+      // const discordSendImage = new Discord.MessageAttachment(image);
+      // msg.channel.send(discordSendImage);
+        // }
       break; 
     case "사냥터": {
       const huntingData = hunting.data;
