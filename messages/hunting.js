e6c7fc74ce1c0414f26b9f99ea76aad43f3fe683
yonghyean <kyhyh0334@gmail.com>
2021-06-11 12:27:21 +0900

    update hunting fields

--- messages/hunting.js
+++ messages/hunting.js
@@ -1,11 +1,29 @@
 const Discord = require('discord.js');
 const huntingJson = require("../hunting.json");
+const cheerio = require("cheerio");
+const getHtml = require("../modules/getHtml");
 
-const hunting = (msg) => {
-  const huntingData = huntingJson.data;
-  const fields = huntingData.map((d) => ({ name: d.level, value: d.desc, inline: false}));
+const hunting = async (msg) => {
+  const { data } = await getHtml('https://maple.gg/info/dungeon/reboot');
+  const $ = cheerio.load(data);
+  const $container = $("section.container.mt-3");
+  const levels = $container.find('.py-2.font-weight-bold');
+  
+  const fields = levels.toArray().map((el) => {
+    const fieldNames = $(el).siblings('div').find('.collapse ul > li b').toArray();
+    const fieldStr = fieldNames.reduce((acc, cur, i) => {
+      return acc + (i !== 0 ? '\n' : '') + $(cur).text();
+    },'');
+    
+    return ({
+      value: fieldStr || '사냥터 없음',
+      name: $(el).text(),
+    })
+  });
+
   const embed = new Discord.MessageEmbed();
-  embed.fields = fields
+  embed.fields = fields;
+
   msg.channel.send(embed);
 }
 
